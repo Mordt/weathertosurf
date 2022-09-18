f7c00e91facc26f149fe3b26eb93092a910d6d45
Carlos Quintero <0xcquintero@gmail.com>
2022-09-18 18:53:40 +1000

    added get request

--- hello.py
+++ hello.py
@@ -9,25 +9,26 @@ request template:
    http://www.bom.gov.au/fwo/ID<STATE>60701/ID<STATE>60701/<STATION_ID>.json
 
 our request is:
-   http://www.bom.gov.au/fwo/IDN60701/IDN60701/IDN11009.json
+   http://www.bom.gov.au/fwo/IDN60701/IDN60701.066051.json
 
 Information obtained from:
 https://stackoverflow.com/questions/39534018/how-to-use-bom-api-for-weather-tide-and-swell
 
 http://www.bom.gov.au/catalogue/anon-ftp.shtml
 
-
+little bay station ID:
+    066051
 
 """
 
-
+"""
 @app.route("/")
 def index():
     return "Hello World"
+"""
 
 @app.route("/", methods = ["GET"])
 def queryBOM():
-    name = request.args.get('name')
-    print name
-    with open()
-
+    r = requests.get('http://www.bom.gov.au/fwo/IDN60701/IDN60701.066051.json')
+    return r.text
+    
