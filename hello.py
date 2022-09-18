3bfbfe37e064694732d8de23a73d9cf72da2fe17
Carlos Quintero <0xcquintero@gmail.com>
2022-09-18 18:22:59 +1000

    added prototype get method plus introductory comments

--- hello.py
+++ hello.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+"""
+IDN for NSW: IDN60701
+IDN for sydney coast: IDN11009
+
+request template:
+   http://www.bom.gov.au/fwo/ID<STATE>60701/ID<STATE>60701/<STATION_ID>.json
+
+our request is:
+   http://www.bom.gov.au/fwo/IDN60701/IDN60701/IDN11009.json
+
+Information obtained from:
+https://stackoverflow.com/questions/39534018/how-to-use-bom-api-for-weather-tide-and-swell
+
+http://www.bom.gov.au/catalogue/anon-ftp.shtml
+
+
+
+"""
+
+
 @app.route("/")
-def hello():
-    return "hello world"
+def index():
+    return "Hello World"
+
+@app.route("/", methods = ["GET"])
+def queryBOM():
+    name = request.args.get('name')
+    print name
+    with open()
+
